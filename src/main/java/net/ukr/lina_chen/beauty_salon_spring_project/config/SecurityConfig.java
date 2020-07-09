5d6f4d853bfa65bd2759f92d4bc0aff39776fb16
sittingonunicorn <sittingonunicorn@users.noreply.github.com>
2020-07-09 17:25:56 +0300

    Front-end

--- src/main/java/net/ukr/lina_chen/beauty_salon_spring_project/config/SecurityConfig.java
+++ src/main/java/net/ukr/lina_chen/beauty_salon_spring_project/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/static/css/**").permitAll()
+                .antMatchers("/", "/css/**", "/error").permitAll()
                 .antMatchers("/registration", "/login", "/index").anonymous()
                 .anyRequest().authenticated()
                 .and()
